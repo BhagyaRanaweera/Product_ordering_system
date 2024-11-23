00f3ce617da11a088fd0045a4c655a4b6b276a53
BhagyaRanaweera <sithubhagya01@gmail.com>
2024-11-23 08:52:58 -0800

    Backend development part

---
+++ src/main/java/com/example/productorderingsystem/ProductorderingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.productorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductorderingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductorderingsystemApplication.class, args);
+	}
+
+}
